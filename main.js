c049f6566b06d1172ffeb5d49671ad9df025fbdb
QKeeper <moncer256710@gmail.com>
2024-01-06 13:05:11 +0500

    update

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp;
 
+const id = tg.initTransferUnsafe.user.id;
+document.querySelector("#id").textContent = id;
 
-const id = tg.initTransferUnsafe.user.id
-document.querySelector(".id").textContent = id
+const username = tg.initTransferUnsafe.user.username;
+document.querySelector("#username").textContent = username;
 
-
-const username = tg.initTransferUnsafe.user.username
-document.querySelector(".username").textContent = username
-
-const name = tg.initTransferUnsafe.user.first_name
-document.querySelector(".name").textContent = name
+const first_name = tg.initTransferUnsafe.user.first_name;
+document.querySelector("#name").textContent = first_name;
