c741c58c0043377042985a8bb048eb1308c22279
QKeeper <moncer256710@gmail.com>
2024-01-06 15:08:30 +0500

    update

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ document.querySelector("#username").textContent = username;
 
 const first_name = tg.initDataUnsafe.user.first_name;
 document.querySelector("#name").textContent = first_name;
+
+data = {
+	mail: document.querySelector("#user-input").textContent,
+	foo: "bar",
+};
+
+document.querySelector("#send").addEventListener("click", () => {
+	tg.sendData(JSON.stringify(data));
+});
